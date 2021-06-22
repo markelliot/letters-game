e9afe67ce9cacd21476b882ae0e62abe614806f1
Mark Elliot <mark.elliot@gmail.com>
2021-06-22 12:27:50 -0400

    upgrade all the things

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,48 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin")
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const path = require('path');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const webpack = require('webpack')
 
 module.exports = {
-  mode: "development",
-  entry: "./src/main/ts/app.tsx",
-  output: {
-    path: path.join(__dirname, "build/assets"),
-    filename: "bundle.js"
-  },
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".scss"]
-  },
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      {
-        test: /\.(sa|sc|c)ss$/i,
-        use: [
+    mode: "development",
+    entry: "./src/main/ts/app.tsx",
+    output: {
+        path: path.join(__dirname, "build/assets"),
+        filename: "bundle.[contenthash].js",
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js", ".scss"],
+        modules: ["node_modules"],
+    },
+    module: {
+        rules: [
+            { test: /\.tsx?$/, loader: "ts-loader" },
             {
-                loader: MiniCssExtractPlugin.loader,
-                options: { publicPath: "../" },
+                test: /\.(sa|sc|c)ss$/i,
+                use: [
+                    "style-loader",
+                    "css-loader",
+                    "sass-loader",
+                ],
             },
-            // "style-loader",
-            "css-loader",
-            "sass-loader",
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                type: "asset/resource",
+            },
         ],
     },
-    {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: ["file-loader"],
+    plugins: [
+        new HtmlWebpackPlugin({
+            path: path.join(__dirname, "build/assets"),
+            filename: "index.html",
+            template: "./src/main/html/index.html",
+        }),
+        new webpack.ProvidePlugin({
+            process: 'process/browser',
+        }),
+    ],
+    devServer: {
+        contentBase: "./build/assets",
+        compress: true,
+        port: 8080,
     },
-    ]
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: "./src/main/html/index.html"
-    }),
-    new MiniCssExtractPlugin({
-      filename: "[name].css",
-  }),
-  ],
-  devServer: {
-    contentBase: "./build/assets",
-    compress: true,
-    port: 8080
-  }
 };
